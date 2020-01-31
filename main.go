0d896d773c82a14ab5f1a8d1e6afaa7e79a49d23
nbrierton001 <nbrierton001@student.butte.edu>
2020-01-31 04:03:47 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+// Programmer name: Nicholas Brierton
+// Date completed:  01302020
+// Description: 
+
+package main
+
+import "fmt" 
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println("My name is Nicholas Brierton, and my major is CSIS.")
+	fmt.Println("This is my first semester at Butte College.")
+	fmt.Println("One of my favorite pastimes is playing guitar.")
+}
+
