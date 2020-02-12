72113544bf279a4b58d1cc33ac613bd109630eaa
nbrierton001 <nbrierton001@student.butte.edu>
2020-02-12 03:46:12 +0000

    lab 1.4.2 mad lib

--- main.go
+++ main.go
@@ -1,15 +1,58 @@
-// Programmer name: Nicholas Brierton
-// Date completed:  01302020
-// Description: 
-
 package main
 
-import "fmt" 
+import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
-	fmt.Println("My name is Nicholas Brierton, and my major is CSIS.")
-	fmt.Println("This is my first semester at Butte College.")
-	fmt.Println("One of my favorite pastimes is playing guitar.")
-}
+    var (
+      nounOne string
+      nounTwo string
+      occupationJobONE string
+      verbOne string
+      placeLocation string
+      nounThree string
+      occupationJobTWO string
+      nounFour string
+      minutesTime string
+      nounFive string
+)
+    fmt.Println("Choose a noun.")
+  
+    fmt.Scanln(&nounOne)
 
+    fmt.Println("Choose another noun.")
+    
+    fmt.Scanln(&nounTwo)
+
+    fmt.Println("Choose a occupation/job.")
+    fmt.Scanln(&occupationJobONE)
+
+    fmt.Println("Choose a verb.")
+    
+    fmt.Scanln(&verbOne)
+
+    fmt.Println("Choose a place or location.")
+    
+    fmt.Scanln(&placeLocation)
+
+    fmt.Println("Choose another noun.")
+    
+    fmt.Scanln(&nounThree)
+
+    fmt.Println("Choose another occupation/job.")
+    
+    fmt.Scanln(&occupationJobTWO)
+
+    fmt.Println("Choose another noun.")
+    
+    fmt.Scanln(&nounFour)
+
+    fmt.Println("Choose a number.")
+    
+    fmt.Scanln(&minutesTime)
+
+    fmt.Println("Choose another noun.")
+    
+    fmt.Scanln(&nounFive)
+
+  fmt.Println("It was during the battle of", nounOne,". A",nounTwo,"went off right next to my platoon. Our", occupationJobONE," yelled us to ", verbOne," to the nearest",placeLocation," we could find. When we got to the",placeLocation, "we waited for our next orders more", nounOne, "continued to explode arround us. Over the radio, our commanding", occupationJobTWO," told us that", nounFour, "support was inbound. we hunkered down and waited for", minutesTime, "minutes until that glorious", nounFour, "support arrived, turning the tide of battle in our favor. We lost many good", nounFive, "that day, but we were victorious in the end.")
+}
